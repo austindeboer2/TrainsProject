5e7c8accd20f03954c9e9565e34322993a987e88
austindeboer2 <austindeboer2@gmail.com>
2023-02-07 10:12:16 -0500

    Connection and TrainNetwork Classes
    Created the Connection and TrainNetwork classes along with their constructors.

---
+++ Trains/Connection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trains
+{
+    /*
+     * Class, Connection, structures a single network connection to be used in 
+     * making the TrainNetwork.
+     */
+    internal class Connection
+    {
+        //Starting town for the train connection
+        private string start { get; }
+        
+        //Ending town for the train connection, cannot be the same as the starting town
+        private string end { get; }
+        
+        //Weight on this connection
+        private int weight { get; }
+
+        /*
+         * Connection Constructor
+         * Parameters:
+         *      String containing the starting town for the connection
+         *      String containing the ending town for the connection
+         *      Int containing the weight of the connection
+         *      
+         *  Function:
+         *      Creates a connection object that has all the needed information for
+         *      that particular route.
+         */
+        public Connection(string start, string end, int weight)
+        {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }   
+
+        /*
+         * Getters for all the private variables
+         */
+        public string GetStart() { return start; }
+
+        public string GetEnd() { return end; }  
+
+        public int GetWeight() { return weight; }
+    }
+}
---
+++ Trains/TrainNetwork.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trains
+{
+    /*
+     * Class, TrainNetwork, contains all the information about the train network.
+     * This information is then used to provide methods to determine route distance, 
+     * unique routes between towns, and the shortest route between towns. 
+     */
+    internal class TrainNetwork
+    {
+        //Array of towns, listed as strings
+        private string[] towns;
+
+        //2D array that keeps track of the weights and routes between towns
+        private int[,] weights;
+
+
+        /*
+         * TrainNetwork Constructor
+         * Parameters:
+         *      List<string> containing all the town names that are included in the network
+         *      List<Connection> containing all of the one way town connections with weights
+         *      
+         *  Function:
+         *      Creates arrays from the arguments to make a network that 
+         *      can be manipulated though the class methods.
+         */
+        public TrainNetwork(List<string> townsL, List<Connection> connections)
+        {
+            towns = townsL.ToArray();
+
+            //Instatiates weights to a square 2D array with sides the same length as towns.
+            //All default values are set to 0.
+            this.weights = new int[connections.Count, connections.Count];
+
+            foreach(Connection c in connections)
+            {
+                int start = 0;
+                int end = 0;
+                int weight = 0;
+
+                //Loops though all the towns to make sure the correct towns are recorded 
+                //to mark the weight in the table.
+                for(int i = 0; i < towns.Length; i++)
+                {
+                    if (c.GetStart().Equals(towns[i])) start = i;
+                    if(c.GetEnd().Equals(towns[i])) end = i;
+                }
+
+                //Checks to make sure that a bad connection was not given with that same
+                //starting and ending location
+                if(start != end)
+                {
+                    //Sets the weight of the connection in the corresponding space in the weights array.
+                    weights[start, end] = weight;
+                }
+
+            }
+        }
+    }
+}
